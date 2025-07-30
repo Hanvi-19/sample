b98948d3979c9f65e2c84857409d354268746458
Hanvi-19 <bigullasoundarya@gmail.com>
2025-07-30 22:32:44 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -3,6 +3,6 @@ class hello
         public static void main(String args[])
         {
                 System.out.println("hello world in sandhya repositoy testbarnch  main");
-                System.out.println("for pollscm made changes in code);
+                System.out.println("for pollscm made changes in code");
         }
 }
