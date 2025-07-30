2c883d085d8e7bf6795fe4b72a8c3e0b8e00b76f
Hanvi-19 <bigullasoundarya@gmail.com>
2025-07-30 22:52:18 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -4,5 +4,8 @@ class hello
         {
                 System.out.println("hello world in sandhya repositoy testbarnch  main");
                 System.out.println("for pollscm made changes in code");
+                System.out.println("webhook ");
+                System.out.println("the moment code is pushed job is triggered in webhook");
+                
         }
 }
